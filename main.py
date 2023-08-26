426d3aaf501b07e41e0b551e4d48ee88de2de956
saifullah rahman <saiflll284@gmail.com>
2023-08-26 09:37:30 +0530

    sd

--- main.py
+++ main.py
@@ -9,20 +9,10 @@ from mysql.connector import Error
 import mysql.connector
 from datetime import datetime
 
-
+# s
 load_dotenv()
 
-# connection = MySQLdb.connect(
-#     host=os.getenv("DB_HOST"),
-#     user=os.getenv("DB_USERNAME"),
-#     passwd=os.getenv("DB_PASSWORD"),
-#     db=os.getenv("DB_NAME"),
-#     autocommit=True,
-#     ssl_mode="VERIFY_IDENTITY",
-#     ssl={
-#         "ca": "/etc/ssl/cert.pem"
-#     }
-# )
+
 
 connection = mysql.connector.connect(
     host=os.getenv("DB_HOST"),
