ec2223b0c4daabbfa4c166f06758bafb23c770f9
saifullah rahman <saiflll284@gmail.com>
2023-08-26 09:21:35 +0530

    f

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-# from utils.s3Functions import put_object, get_signed_url
+from utils.s3Functions import put_object, get_signed_url
 import json
 
 from dotenv import load_dotenv
@@ -24,30 +24,30 @@ load_dotenv()
 #     }
 # )
 
-connection = mysql.connector.connect(
-    host=os.getenv("DB_HOST"),
-    database=os.getenv("DB_NAME"),
-    user=os.getenv("DB_USERNAME"),
-    password=os.getenv("DB_PASSWORD"),
-    ssl_ca=os.getenv("SSL_CERT")
-)
+# connection = mysql.connector.connect(
+#     host=os.getenv("DB_HOST"),
+#     database=os.getenv("DB_NAME"),
+#     user=os.getenv("DB_USERNAME"),
+#     password=os.getenv("DB_PASSWORD"),
+#     ssl_ca=os.getenv("SSL_CERT")
+# )
 
 
-try:
-    if connection.is_connected():
-        cursor = connection.cursor()
-    cursor.execute("select @@version ")
-    version = cursor.fetchone()
-    if version:
-        print('Running version: ', version)
-    else:
-        print('Not connected.')
-    # get all the tables
-    # cursor.execute("select * from uploaded_files")
-    # tables = cursor.fetchall()
-    # print(tables)
-except Error as e:
-    print("Error while connecting to MySQL", e)
+# try:
+#     if connection.is_connected():
+#         cursor = connection.cursor()
+#     cursor.execute("select @@version ")
+#     version = cursor.fetchone()
+#     if version:
+#         print('Running version: ', version)
+#     else:
+#         print('Not connected.')
+#     # get all the tables
+#     # cursor.execute("select * from uploaded_files")
+#     # tables = cursor.fetchall()
+#     # print(tables)
+# except Error as e:
+#     print("Error while connecting to MySQL", e)
 
 
 app = Flask(__name__)
