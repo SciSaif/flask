1d7cf8762266cd31ccf14d890e02c8dfcc5ee870
saifullah rahman <saiflll284@gmail.com>
2023-08-26 09:30:37 +0530

    c

--- main.py
+++ main.py
@@ -24,30 +24,30 @@ load_dotenv()
 #     }
 # )
 
-# connection = mysql.connector.connect(
-#     host=os.getenv("DB_HOST"),
-#     database=os.getenv("DB_NAME"),
-#     user=os.getenv("DB_USERNAME"),
-#     password=os.getenv("DB_PASSWORD"),
-#     ssl_ca=os.getenv("SSL_CERT")
-# )
+connection = mysql.connector.connect(
+    host=os.getenv("DB_HOST"),
+    database=os.getenv("DB_NAME"),
+    user=os.getenv("DB_USERNAME"),
+    password=os.getenv("DB_PASSWORD"),
+    ssl_ca=os.getenv("SSL_CERT")
+)
 
 
-# try:
-#     if connection.is_connected():
-#         cursor = connection.cursor()
-#     cursor.execute("select @@version ")
-#     version = cursor.fetchone()
-#     if version:
-#         print('Running version: ', version)
-#     else:
-#         print('Not connected.')
-#     # get all the tables
-#     # cursor.execute("select * from uploaded_files")
-#     # tables = cursor.fetchall()
-#     # print(tables)
-# except Error as e:
-#     print("Error while connecting to MySQL", e)
+try:
+    if connection.is_connected():
+        cursor = connection.cursor()
+    cursor.execute("select @@version ")
+    version = cursor.fetchone()
+    if version:
+        print('Running version: ', version)
+    else:
+        print('Not connected.')
+    # get all the tables
+    # cursor.execute("select * from uploaded_files")
+    # tables = cursor.fetchall()
+    # print(tables)
+except Error as e:
+    print("Error while connecting to MySQL", e)
 
 
 app = Flask(__name__)
